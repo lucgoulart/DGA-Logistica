5a14636344e792b82e83d8dda466aa57a13bcc1a
Lucas Goulart Salman <lucas.salman@engdb.com.br>
2025-06-02 10:32:59 -0300

    pagina parceiros

--- src/app/components/parceiros/parceiros/parceiros.component.ts
+++ src/app/components/parceiros/parceiros/parceiros.component.ts
@@ -6,10 +6,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./parceiros.component.scss']
 })
 export class ParceirosComponent implements OnInit {
+  imagens = [
+    'assets/parceiro1.jpeg',
+    'assets/parceiro2.jpeg',
+    'assets/parceiro3.jpeg',
+    'assets/parceiro4.jpeg',
+    'assets/parceiro1.jpeg',
+    'assets/parceiro2.jpeg',
+    'assets/parceiro3.jpeg',
+    'assets/parceiro4.jpeg'
+  ];
 
-  constructor() { }
+  imagensDuplicadas: string[] = [];
 
   ngOnInit(): void {
+    this.imagensDuplicadas = [...this.imagens, ...this.imagens];
   }
-
 }
