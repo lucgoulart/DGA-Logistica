c0cdb66d4126ee31b564cdbe9127cba631766ee4
Lucas Goulart Salman <lucas.salman@engdb.com.br>
2025-10-14 16:50:26 -0300

    codigo de conduta footer

--- src/app/components/footer/footer/footer.component.ts
+++ src/app/components/footer/footer/footer.component.ts
@@ -11,5 +11,10 @@ export class FooterComponent implements OnInit {
 
   ngOnInit(): void {
   }
-
+    downloadCodigoConduta() {
+    const link = document.createElement('a');
+    link.href = 'assets/codigo-conduta.pdf'; // caminho do PDF na pasta assets
+    link.download = 'codigo-conduta.pdf';
+    link.click();
+  }
 }
